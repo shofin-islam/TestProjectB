6bd3036dd6d06f67a7806d9557b57bca01545d2c
Safiul Islam BS23 <safiulislam@Safiuls-MacBook-Pro.local>
2020-09-27 10:56:01 +0600

    trigger to maven by commit

--- TestOne.java
+++ TestOne.java
@@ -3,6 +3,11 @@ public class TestOne {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World..Test");
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello World..Test"+i);
+		}
+		
 
 	}
 
